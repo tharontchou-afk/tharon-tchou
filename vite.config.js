7210178f9928144e926a8ce949025d420811c65e
djembadante <romain.stempfel@gmail.com>
2026-07-09 15:41:25 +0200

    configure Vite multi-page buld

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        billetterie: resolve(__dirname, "billetterie.html"),
+        cgv: resolve(__dirname, "cgv.html"),
+        confidentialite: resolve(__dirname, "confidentialite.html"),
+        infosSociete: resolve(__dirname, "infos-societe.html"),
+        mentionsLegales: resolve(__dirname, "mentions-legales.html"),
+      },
+    },
+  },
+});
