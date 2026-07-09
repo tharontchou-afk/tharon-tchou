c3d2ded9f5a327c5068f88d27c30b9d2f9d8b329
djembadante <romain.stempfel@gmail.com>
2026-07-09 14:31:53 +0200

    intégration pages légales et mise à jour sitemap

--- src/js/intro.js
+++ src/js/intro.js
@@ -1,9 +1,19 @@
 const introOverlay = document.querySelector("#intro-overlay");
+const hasSeenIntro = sessionStorage.getItem("introSeen");
 
 if (introOverlay) {
-  window.addEventListener("load", () => {
-    setTimeout(() => {
-      introOverlay.classList.add("is-hidden");
-    }, 800);
-  });
+  if (hasSeenIntro) {
+    introOverlay.remove();
+  } else {
+    window.addEventListener("load", () => {
+      setTimeout(() => {
+        introOverlay.classList.add("is-hidden");
+        sessionStorage.setItem("introSeen", "true");
+
+        setTimeout(() => {
+          introOverlay.remove();
+        }, 600); // durée de la transition CSS
+      }, 800);
+    });
+  }
 }
